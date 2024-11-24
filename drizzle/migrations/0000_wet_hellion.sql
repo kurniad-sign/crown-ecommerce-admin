98c9e352e21d0706b448466d7830f6edea59a5be
Agung Kurniadi <agungkurniadi99@gmail.com>
2024-11-24 21:43:13 +0700

    feat: new db migration

--- drizzle/migrations/0000_wet_hellion.sql
+++ drizzle/migrations/0000_wet_hellion.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS "categories" (
-	"id" uuid DEFAULT gen_random_uuid(),
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"parent_id" uuid DEFAULT gen_random_uuid(),
 	"name" varchar(100) NOT NULL,
 	"store_id" uuid DEFAULT gen_random_uuid() NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "categories_id_unique" UNIQUE("id")
+	CONSTRAINT "categories_parent_id_unique" UNIQUE("parent_id")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "stores" (
@@ -19,13 +20,13 @@ CREATE TABLE IF NOT EXISTS "stores" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "categories" ADD CONSTRAINT "categories_store_id_stores_id_fk" FOREIGN KEY ("store_id") REFERENCES "public"."stores"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "categories" ADD CONSTRAINT "categories_parent_id_categories_id_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "categories" ADD CONSTRAINT "custom_fk" FOREIGN KEY ("id") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "categories" ADD CONSTRAINT "categories_store_id_stores_id_fk" FOREIGN KEY ("store_id") REFERENCES "public"."stores"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
