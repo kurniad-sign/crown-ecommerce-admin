3228351b43766d321dfb3a621bba5bdc3e025323
Agung Kurniadi <agungkurniadi99@gmail.com>
2024-11-25 17:37:05 +0700

    feat: new migration file

--- drizzle/migrations/0000_romantic_daimon_hellstrom.sql
+++ drizzle/migrations/0000_romantic_daimon_hellstrom.sql
@@ -1,22 +1,20 @@
 CREATE TABLE IF NOT EXISTS "categories" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"parent_id" uuid DEFAULT gen_random_uuid(),
+	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+	"parent_id" uuid DEFAULT uuid_generate_v4(),
 	"name" varchar(100) NOT NULL,
-	"store_id" uuid DEFAULT gen_random_uuid() NOT NULL,
+	"store_id" uuid DEFAULT uuid_generate_v4() NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "categories_parent_id_unique" UNIQUE("parent_id")
+	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "stores" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" varchar(255) NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+	"name" varchar(100) NOT NULL,
 	"user_id" varchar(255) NOT NULL,
-	"storeId" varchar(255),
+	"unique_store" varchar(50),
 	"store_logo_url" varchar(255),
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "stores_storeId_unique" UNIQUE("storeId")
+	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 DO $$ BEGIN
